3d85afd68b488d2a734febd16856a92bb5aba0a3
ogamespec <ogamespec@gmail.com>
2022-03-12 11:45:15 +0300

    LS Sim

---
+++ Breaknes/BreaksCore/LS139.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+namespace LS
+{
+	void LS139::sim(BaseLogic::TriState inputs[], BaseLogic::TriState n_Y1[4], BaseLogic::TriState n_Y2[4])
+	{
+
+	}
+}
---
+++ Breaknes/BreaksCore/LS139.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace LS
+{
+	enum class LS139_Input
+	{
+		n_EN1 = 0,
+		n_EN2,
+		A1_0,
+		A1_1,
+		A2_0,
+		A2_1,
+		Max,
+	};
+
+	class LS139
+	{
+	public:
+		static void sim(BaseLogic::TriState inputs[], BaseLogic::TriState n_Y1[4], BaseLogic::TriState n_Y2[4]);
+	};
+}
