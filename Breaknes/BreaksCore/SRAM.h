df90223e5d5663c6a3f2d927a6bde516f9cd5893
ogamespec <ogamespec@gmail.com>
2022-03-12 19:46:13 +0300

    SRAM

--- Breaknes/BreaksCore/SRAM.h
+++ Breaknes/BreaksCore/SRAM.h
@@ -1,1 +1,17 @@
+// Generic SRAM
+
 #pragma once
+
+namespace Breaknes
+{
+	class SRAM
+	{
+		uint8_t* mem = nullptr;
+
+	public:
+		SRAM(size_t bits);
+		~SRAM();
+
+		void sim(BaseLogic::TriState n_CS, BaseLogic::TriState n_WE, BaseLogic::TriState n_OE, uint32_t *addr, uint8_t *data, bool& dz);
+	};
+}
