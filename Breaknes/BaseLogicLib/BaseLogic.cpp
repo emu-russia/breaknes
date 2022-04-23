49e1f93ba9f4b4ad37efcc647e15d8bd39de96ff
ogamespec <ogamespec@gmail.com>
2022-04-23 22:06:12 +0300

    NOR-10

--- Breaknes/BaseLogicLib/BaseLogic.cpp
+++ Breaknes/BaseLogicLib/BaseLogic.cpp
@@ -55,6 +55,11 @@ namespace BaseLogic
 		return (TriState)((~(in[0] | in[1] | in[2] | in[3] | in[4] | in[5] | in[6] | in[7] | in[8])) & 1);
 	}
 
+	TriState NOR10(TriState in[10])
+	{
+		return (TriState)((~(in[0] | in[1] | in[2] | in[3] | in[4] | in[5] | in[6] | in[7] | in[8] | in[9])) & 1);
+	}
+
 	TriState NOR13(TriState in[13])
 	{
 		return (TriState)((~(in[0] | in[1] | in[2] | in[3] | in[4] | in[5] | in[6] | in[7] | in[8] | in[9] | in[10] | in[11] | in[12])) & 1);
