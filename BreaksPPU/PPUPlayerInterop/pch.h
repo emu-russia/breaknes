ed9f5f9d14dfcb57af28ad8c280ec7cd955db349
ogamespec <ogamespec@gmail.com>
2022-04-25 22:16:45 +0300

    PPUPlayerInterop skeleton

---
+++ BreaksPPU/PPUPlayerInterop/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+
+#include <cstdint>
+
+#include "../../Breaknes/BaseLogicLib/BaseLogic.h"
+#include "../PPUSim/ppu.h"
