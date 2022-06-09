85621a56e3e9540e1d5fdcc0e495c671d7f0b89b
ogamespec <ogamespec@gmail.com>
2022-06-09 21:58:46 +0300

    FormRawViewer

--- BreaksPPU/PPUPlayer/FormRawViewer.Designer.cs
+++ BreaksPPU/PPUPlayer/FormRawViewer.Designer.cs
@@ -28,21 +28,78 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+			this.splitContainer1.Size = new System.Drawing.Size(729, 385);
+			this.splitContainer1.SplitterDistance = 32;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 6);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 23);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(729, 349);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
 			// 
 			// FormRawViewer
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(729, 385);
+			this.Controls.Add(this.splitContainer1);
 			this.KeyPreview = true;
 			this.Name = "FormRawViewer";
 			this.Text = "RAW Viewer";
 			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormRawViewer_KeyDown);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
--- BreaksPPU/PPUPlayer/FormRawViewer.cs
+++ BreaksPPU/PPUPlayer/FormRawViewer.cs
@@ -14,6 +14,18 @@ namespace PPUPlayer
 {
 	public partial class FormRawViewer : Form
 	{
+		PPUPlayerInterop.VideoSignalFeatures ppu_features;
+		int SamplesPerScan;
+
+		PPUPlayerInterop.VideoOutSample[] ScanBuffer;
+		int WritePtr = 0;
+		bool SyncFound = false;
+		int SyncPos = -1;
+
+		Color[] field = new Color[256 * 240];
+		int CurrentScan = 0;
+		List<Bitmap> fields = new List<Bitmap>();
+
 		public FormRawViewer(string filename)
 		{
 			InitializeComponent();
@@ -24,6 +36,28 @@ namespace PPUPlayer
 			UInt16[] dump = LogisimHEXConv.HEXToShortArray(text);
 
 			// Process the dump field by field and fill the ComboBox and PictureView.
+
+			ResetVisualize();
+
+			comboBox1.Items.Clear();
+
+			for (int i = 0; i < dump.Length; i++)
+			{
+				PPUPlayerInterop.VideoOutSample sample = new();
+				sample.raw = dump[i];
+				ProcessSample(sample);
+			}
+
+			if (fields.Count != 0)
+			{
+				ShowField(0);
+			}
+			else
+			{
+				AddField();
+				Console.WriteLine("Added partial field.");
+				ShowField(0);
+			}
 		}
 
 		private void FormRawViewer_KeyDown(object sender, KeyEventArgs e)
@@ -32,6 +66,132 @@ namespace PPUPlayer
 			{
 				Close();
 			}
+		}
+
+		void ResetVisualize()
+		{
+			PPUPlayerInterop.GetSignalFeatures(out ppu_features);
+
+			// In Logisim the samples are sampled every CLK, not every half-cycle as in combat. Therefore, you must divide by 2.
+			SamplesPerScan = ppu_features.PixelsPerScan * ppu_features.SamplesPerPCLK / 2;
+			ScanBuffer = new PPUPlayerInterop.VideoOutSample[2 * SamplesPerScan];
+			WritePtr = 0;
+
+			SyncFound = false;
+			SyncPos = -1;
+			CurrentScan = 0;
+		}
+
+		void ProcessSample(PPUPlayerInterop.VideoOutSample sample)
+		{
+			ScanBuffer[WritePtr] = sample;
+
+			// Check that the sample is HSync.
+
+			bool sync = false;
+			sync = (sample.raw & 0b1000000000) != 0;
+
+			// If the beginning of HSync is found - remember its offset in the input buffer.
+
+			if (sync && !SyncFound)
+			{
+				SyncPos = WritePtr;
+				SyncFound = true;
+			}
+
+			// Advance write pointer
+
+			WritePtr++;
+
+			// If HSync is found and the number of samples is more than enough, process the scan.
+
+			if (SyncFound && (SyncPos + WritePtr) >= SamplesPerScan)
+			{
+				ProcessScanRAW();
+
+				SyncFound = false;
+				WritePtr = 0;
+			}
+		}
+
+		void ProcessScanRAW()
+		{
+			int ReadPtr = SyncPos;
+
+			// In Logisim the samples are sampled every CLK, not every half-cycle as in combat. Therefore, you must divide by 2.
+			int SamplesPerPixelInLogisimDump = ppu_features.SamplesPerPCLK / 2;
+
+			// Skip HSync and Back Porch
+
+			while ((ScanBuffer[ReadPtr].raw & 0b1000000000) != 0)
+			{
+				ReadPtr++;
+			}
+
+			ReadPtr += ppu_features.BackPorchSize * SamplesPerPixelInLogisimDump;
+
+			// Output the visible part of the signal
+
+			for (int i = 0; i < 256; i++)
+			{
+				if (CurrentScan < 240)
+				{
+					byte r, g, b;
+					PPUPlayerInterop.ConvertRAWToRGB(ScanBuffer[ReadPtr].raw, out r, out g, out b);
+
+					field[CurrentScan * 256 + i] = Color.FromArgb(r, g, b);
+				}
+
+				ReadPtr += SamplesPerPixelInLogisimDump;
+			}
+
+			CurrentScan++;
+			if (CurrentScan >= ppu_features.ScansPerField)
+			{
+				AddField();
+				Console.WriteLine("Added Field");
+				CurrentScan = 0;
+			}
+		}
+
+		void AddField()
+		{
+			int w = 256;
+			int h = 240;
+
+			comboBox1.Items.Add(fields.Count.ToString());
+
+			Bitmap? field_pic = new(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+			Bitmap pic = field_pic;
+			Graphics gr = Graphics.FromImage(pic);
+
+			for (int y = 0; y < h; y++)
+			{
+				for (int x = 0; x < w; x++)
+				{
+					Color p = field[w * y + x];
+					gr.FillRectangle(new SolidBrush(p), x, y, 1, 1);
+				}
+			}
+
+			fields.Add(field_pic);
+			gr.Dispose();
+		}
+
+		void ShowField(int n)
+		{
+			if (n < fields.Count)
+			{
+				pictureBox1.Image = fields[n];
+				Console.WriteLine("Show Field: " + n.ToString());
+			}
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int n = comboBox1.SelectedIndex;
+			ShowField(n);
 		}
 	}
 }
