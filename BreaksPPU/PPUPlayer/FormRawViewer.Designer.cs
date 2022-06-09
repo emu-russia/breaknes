eef8626a7dd80289b6a0ac1fbe033d269a4c8d37
ogamespec <ogamespec@gmail.com>
2022-06-09 20:24:23 +0300

    FormRawViewer skeleton

---
+++ BreaksPPU/PPUPlayer/FormRawViewer.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PPUPlayer
+{
+	partial class FormRawViewer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// FormRawViewer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(729, 385);
+			this.KeyPreview = true;
+			this.Name = "FormRawViewer";
+			this.Text = "RAW Viewer";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormRawViewer_KeyDown);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
