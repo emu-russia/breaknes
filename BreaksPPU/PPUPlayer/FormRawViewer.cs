b2c2fab144e7c31e88ac2f612e11201f40a044a4
ogamespec <ogamespec@gmail.com>
2022-06-09 21:30:19 +0300

    LogisimHEXConv

--- BreaksPPU/PPUPlayer/FormRawViewer.cs
+++ BreaksPPU/PPUPlayer/FormRawViewer.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
 namespace PPUPlayer
 {
 	public partial class FormRawViewer : Form
@@ -15,6 +17,13 @@ namespace PPUPlayer
 		public FormRawViewer(string filename)
 		{
 			InitializeComponent();
+
+			// Load the Logisim dump
+
+			string text = File.ReadAllText(filename);
+			UInt16[] dump = LogisimHEXConv.HEXToShortArray(text);
+
+			// Process the dump field by field and fill the ComboBox and PictureView.
 		}
 
 		private void FormRawViewer_KeyDown(object sender, KeyEventArgs e)
---
+++ BreaksPPU/PPUPlayer/LogisimHEXConv.cs
@@ -0,0 +1,51 @@
+﻿// Converts Logisim .hex dump to UInt16 array.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+namespace PPUPlayer
+{
+	internal class LogisimHEXConv
+	{
+		public static UInt16 [] HEXToShortArray(string text)
+		{
+			List<UInt16> res = new List<UInt16>();
+
+            Regex regex = new Regex(@"^v2.0 raw[\r\n]+([\da-f\*]+\s*)*");
+
+            var matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                foreach (var cap in match.Groups[1].Captures)
+				{
+                    string entry = cap.ToString();
+
+                    if (entry.Contains('*'))
+					{
+						string[] ab = entry.Trim().Split('*');
+						int counter = Convert.ToInt32(ab[0], 10);
+						UInt16 val = Convert.ToUInt16(ab[1], 16);
+						for (int i=0; i<counter; i++)
+						{
+							res.Add(val);
+						}
+					}
+					else
+					{
+						UInt16 val = Convert.ToUInt16(entry.Trim(), 16);
+						res.Add(val);
+					}
+				}
+            }
+
+            return res.ToArray();
+		}
+
+	}
+}
