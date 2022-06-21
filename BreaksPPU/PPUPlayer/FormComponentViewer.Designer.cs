8dd37304e4b49b304eb8de48bff18e1b700791be
ogamespec <ogamespec@gmail.com>
2022-06-21 21:57:17 +0300

    FormComponentViewer

---
+++ BreaksPPU/PPUPlayer/FormComponentViewer.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PPUPlayer
+{
+	partial class FormComponentViewer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+			this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+			this.splitContainer1.SplitterDistance = 32;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 6);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 23);
+			this.comboBox1.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 414);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// FormComponentViewer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.splitContainer1);
+			this.KeyPreview = true;
+			this.Name = "FormComponentViewer";
+			this.Text = "Component Viewer";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormComponentViewer_KeyDown);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
---
+++ BreaksPPU/PPUPlayer/FormComponentViewer.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PPUPlayer
+{
+	public partial class FormComponentViewer : Form
+	{
+		PPUPlayerInterop.VideoSignalFeatures ppu_features;
+		int SamplesPerScan;
+
+		PPUPlayerInterop.VideoOutSample[]? ScanBuffer;
+		int WritePtr = 0;
+		bool SyncFound = false;
+		int SyncPos = -1;
+
+		Color[] field = new Color[256 * 240];
+		int CurrentScan = 0;
+		List<Bitmap> fields = new List<Bitmap>();
+
+		public FormComponentViewer(string filename)
+		{
+			InitializeComponent();
+
+			// Load the Logisim dump
+
+			string text = File.ReadAllText(filename);
+			UInt32[] dump = LogisimHEXConv.HEXToUIntArray(text);
+
+			// Process the dump field by field and fill the ComboBox and PictureView.
+
+			ResetVisualize();
+
+			comboBox1.Items.Clear();
+
+			for (int i = 0; i < dump.Length; i++)
+			{
+				PPUPlayerInterop.VideoOutSample sample = new();
+
+				sample.RED = (byte)(dump[i] & 0xff);
+				sample.GREEN = (byte)((dump[i] >> 8) & 0xff);
+				sample.BLUE = (byte)((dump[i] >> 16) & 0xff);
+				sample.nSYNC = (byte)((dump[i] >> 24) & 0xff);
+
+				ProcessSample(sample);
+			}
+
+			if (fields.Count != 0)
+			{
+				ShowField(0);
+			}
+			else
+			{
+				AddField();
+				Console.WriteLine("Added partial field.");
+				ShowField(0);
+			}
+		}
+
+		void ResetVisualize()
+		{
+			PPUPlayerInterop.GetSignalFeatures(out ppu_features);
+
+			SamplesPerScan = ppu_features.PixelsPerScan * ppu_features.SamplesPerPCLK;
+			ScanBuffer = new PPUPlayerInterop.VideoOutSample[2 * SamplesPerScan];
+			WritePtr = 0;
+
+			SyncFound = false;
+			SyncPos = -1;
+			CurrentScan = 0;
+		}
+
+		void ProcessSample(PPUPlayerInterop.VideoOutSample sample)
+		{
+			ScanBuffer[WritePtr] = sample;
+
+			// Check that the sample is HSync.
+
+			bool sync = false;
+			sync = sample.nSYNC == 0;
+
+			// If the beginning of HSync is found - remember its offset in the input buffer.
+
+			if (sync && !SyncFound)
+			{
+				SyncPos = WritePtr;
+				SyncFound = true;
+			}
+
+			// Advance write pointer
+
+			WritePtr++;
+
+			// If HSync is found and the number of samples is more than enough, process the scan.
+
+			if (SyncFound && (SyncPos + WritePtr) >= SamplesPerScan)
+			{
+				ProcessScanRGB();
+
+				SyncFound = false;
+				WritePtr = 0;
+			}
+		}
+
+		void ProcessScanRGB()
+		{
+			int ReadPtr = SyncPos;
+
+			// Skip HSync and Back Porch
+
+			while (ScanBuffer[ReadPtr].nSYNC == 0)
+			{
+				ReadPtr++;
+
+				if (ReadPtr >= ScanBuffer.Length)
+					break;
+			}
+
+			if (ReadPtr >= ScanBuffer.Length)
+			{
+				return;
+			}
+
+			ReadPtr += ppu_features.BackPorchSize * ppu_features.SamplesPerPCLK;
+
+			// Output the visible part of the signal
+
+			for (int i = 0; i < 256; i++)
+			{
+				if (CurrentScan < 240)
+				{
+					int red = 0, green = 0, blue = 0;
+
+					for (int n = 0; n < ppu_features.SamplesPerPCLK; n++)
+					{
+						var sample = ScanBuffer[ReadPtr];
+						red += sample.RED;
+						green += sample.GREEN;
+						blue += sample.BLUE;
+						ReadPtr++;
+					}
+
+					byte r = (byte)(red / ppu_features.SamplesPerPCLK);
+					byte g = (byte)(green / ppu_features.SamplesPerPCLK);
+					byte b = (byte)(blue / ppu_features.SamplesPerPCLK);
+
+					field[CurrentScan * 256 + i] = Color.FromArgb(r, g, b);
+				}
+			}
+
+			CurrentScan++;
+			if (CurrentScan >= ppu_features.ScansPerField)
+			{
+				AddField();
+				Console.WriteLine("Added Field");
+				CurrentScan = 0;
+			}
+		}
+
+		void AddField()
+		{
+			int w = 256;
+			int h = 240;
+
+			comboBox1.Items.Add(fields.Count.ToString());
+
+			Bitmap? field_pic = new(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+			Bitmap pic = field_pic;
+			Graphics gr = Graphics.FromImage(pic);
+
+			for (int y = 0; y < h; y++)
+			{
+				for (int x = 0; x < w; x++)
+				{
+					Color p = field[w * y + x];
+					gr.FillRectangle(new SolidBrush(p), x, y, 1, 1);
+				}
+			}
+
+			fields.Add(field_pic);
+			gr.Dispose();
+		}
+
+		void ShowField(int n)
+		{
+			if (n < fields.Count)
+			{
+				pictureBox1.Image = fields[n];
+				Console.WriteLine("Show Field: " + n.ToString());
+			}
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int n = comboBox1.SelectedIndex;
+			ShowField(n);
+		}
+
+		private void FormComponentViewer_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				Close();
+			}
+		}
+	}
+}
