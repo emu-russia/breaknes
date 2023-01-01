0f3b32ba6423fe0e50fe5f510b28a03380e429b6
ogamespec <ogamespec@gmail.com>
2023-01-02 01:06:54 +0300

    env.cpp

--- BreaksAPU/APUSim/env.h
+++ BreaksAPU/APUSim/env.h
@@ -11,10 +11,21 @@ namespace APUSim
 
 		APU* apu = nullptr;
 
+		RegisterBit envdis_reg{};
+		RegisterBit lc_reg{};
+		RegisterBit vol_reg[4]{};
+		DownCounterBit decay_cnt[4]{};
+		DownCounterBit env_cnt[4]{};
+		BaseLogic::FF EnvReload{};
+		BaseLogic::DLatch erld_latch{};
+		BaseLogic::DLatch reload_latch{};
+		BaseLogic::DLatch rco_latch{};
+		BaseLogic::DLatch eco_latch{};
+
 	public:
 		EnvelopeUnit(APU* parent);
 		~EnvelopeUnit();
 
-		void sim();
+		void sim(BaseLogic::TriState V[4], BaseLogic::TriState WR_Reg, BaseLogic::TriState WR_LC, BaseLogic::TriState& LC);
 	};
 }
