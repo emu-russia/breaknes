a559451dec2e5dc2990ccf1ca4a96faa57f39446
ogamespec <ogamespec@gmail.com>
2023-02-04 22:13:46 +0300

    DSoundDemo

---
+++ Demos/DSoundDemo/FormMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace DSoundDemo
+{
+	partial class FormMain
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.loadwavFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.signalPlot1 = new System.Windows.Forms.SignalPlot();
+			this.furryPlot1 = new System.Windows.Forms.FurryPlot();
+			this.menuStrip1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadwavFileToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// loadwavFileToolStripMenuItem
+			// 
+			this.loadwavFileToolStripMenuItem.Name = "loadwavFileToolStripMenuItem";
+			this.loadwavFileToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+			this.loadwavFileToolStripMenuItem.Text = "Load .wav File...";
+			this.loadwavFileToolStripMenuItem.Click += new System.EventHandler(this.loadwavFileToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(154, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "Help";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+			this.aboutToolStripMenuItem.Text = "About";
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.DefaultExt = "wav";
+			this.openFileDialog1.Filter = "Wave files|*.wav|All files|*.*";
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(33, 22);
+			this.toolStripButton1.Text = "Play";
+			this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 49);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.signalPlot1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.furryPlot1);
+			this.splitContainer1.Size = new System.Drawing.Size(800, 401);
+			this.splitContainer1.SplitterDistance = 184;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// signalPlot1
+			// 
+			this.signalPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.signalPlot1.FillColor = System.Drawing.Color.Black;
+			this.signalPlot1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.signalPlot1.GridColor = System.Drawing.Color.Green;
+			this.signalPlot1.LabelsColor = System.Drawing.Color.White;
+			this.signalPlot1.Location = new System.Drawing.Point(0, 0);
+			this.signalPlot1.Name = "signalPlot1";
+			this.signalPlot1.SignalColor = System.Drawing.Color.SpringGreen;
+			this.signalPlot1.Size = new System.Drawing.Size(800, 184);
+			this.signalPlot1.TabIndex = 0;
+			this.signalPlot1.Text = "signalPlot1";
+			this.signalPlot1.ZeroColor = System.Drawing.Color.Red;
+			// 
+			// furryPlot1
+			// 
+			this.furryPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.furryPlot1.Location = new System.Drawing.Point(0, 0);
+			this.furryPlot1.Name = "furryPlot1";
+			this.furryPlot1.Size = new System.Drawing.Size(800, 213);
+			this.furryPlot1.TabIndex = 0;
+			this.furryPlot1.Text = "furryPlot1";
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "FormMain";
+			this.Text = "DSound Demo";
+			this.Load += new System.EventHandler(this.FormMain_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem loadwavFileToolStripMenuItem;
+		private ToolStripSeparator toolStripSeparator1;
+		private ToolStripMenuItem exitToolStripMenuItem;
+		private OpenFileDialog openFileDialog1;
+		private ToolStrip toolStrip1;
+		private ToolStripButton toolStripButton1;
+		private SplitContainer splitContainer1;
+		private ToolStripMenuItem helpToolStripMenuItem;
+		private ToolStripMenuItem aboutToolStripMenuItem;
+		private SignalPlot signalPlot1;
+		private FurryPlot furryPlot1;
+	}
+}
---
+++ Demos/DSoundDemo/FormMain.cs
@@ -0,0 +1,260 @@
+using Microsoft.VisualBasic;
+using SharpDX;
+using SharpDX.DirectSound;
+using SharpDX.Multimedia;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace DSoundDemo
+{
+	public partial class FormMain : Form
+	{
+		DirectSound directSound;
+		PrimarySoundBuffer primarySoundBuffer;
+		SecondarySoundBuffer secondarySoundBuffer;
+		WaveFormat waveFormat;
+
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void FormMain_Load(object sender, EventArgs e)
+		{
+			InitDSound();
+
+			PaintSound();
+		}
+
+		// https://github.com/sharpdx/SharpDX-Samples/blob/master/Desktop/DirectSound/PlaySound/Program.cs
+
+		private void InitDSound()
+		{
+			directSound = new DirectSound();
+
+			directSound.SetCooperativeLevel(Handle, CooperativeLevel.Priority);
+
+			// Create PrimarySoundBuffer
+			var primaryBufferDesc = new SoundBufferDescription();
+			primaryBufferDesc.Flags = BufferFlags.PrimaryBuffer;
+			primaryBufferDesc.AlgorithmFor3D = Guid.Empty;
+
+			primarySoundBuffer = new PrimarySoundBuffer(directSound, primaryBufferDesc);
+
+			// Play the PrimarySound Buffer
+			primarySoundBuffer.Play(0, PlayFlags.Looping);
+
+			// Default WaveFormat Stereo 44100 16 bit
+			waveFormat = new WaveFormat();
+
+			// Create SecondarySoundBuffer
+			var secondaryBufferDesc = new SoundBufferDescription();
+			secondaryBufferDesc.BufferBytes = waveFormat.ConvertLatencyToByteSize(60000);
+			secondaryBufferDesc.Format = waveFormat;
+			secondaryBufferDesc.Flags = BufferFlags.GetCurrentPosition2 | BufferFlags.ControlPositionNotify | BufferFlags.GlobalFocus |
+										BufferFlags.ControlVolume | BufferFlags.StickyFocus;
+			secondaryBufferDesc.AlgorithmFor3D = Guid.Empty;
+			secondarySoundBuffer = new SecondarySoundBuffer(directSound, secondaryBufferDesc);
+		}
+
+		private void PaintSound()
+		{
+			// Get Capabilties from secondary sound buffer
+			var capabilities = secondarySoundBuffer.Capabilities;
+
+			// Lock the buffer
+			DataStream dataPart2;
+			var dataPart1 = secondarySoundBuffer.Lock(0, capabilities.BufferBytes, LockFlags.EntireBuffer, out dataPart2);
+
+			// Fill the buffer with some sound
+			int numberOfSamples = capabilities.BufferBytes / waveFormat.BlockAlign;
+
+			int plot_samples_count = Math.Min(numberOfSamples, 4096);
+			float[] plot_samples = new float[plot_samples_count];
+
+			for (int i = 0; i < numberOfSamples; i++)
+			{
+				double vibrato = Math.Cos(2 * Math.PI * 10.0 * i / waveFormat.SampleRate);
+				short value = (short)(Math.Cos(2 * Math.PI * (220.0 + 4.0 * vibrato) * i / waveFormat.SampleRate) * 16384); // Not too loud
+				
+				if (i < plot_samples_count)
+				{
+					plot_samples[i] = value;
+				}
+
+				dataPart1.Write(value);
+				dataPart1.Write(value);
+			}
+
+			signalPlot1.PlotSignal(plot_samples);
+
+			// Unlock the buffer
+			secondarySoundBuffer.Unlock(dataPart1, dataPart2);
+		}
+
+		private void PaintWavFile (string wav_name)
+		{
+			float[] L;
+			float[] R;
+
+			bool res = ReadWav(wav_name, out L, out R);
+			if (!res)
+			{
+				MessageBox.Show("ReadWav cant");
+				return;
+			}
+
+			// Create SecondarySoundBuffer
+			var secondaryBufferDesc = new SoundBufferDescription();
+			secondaryBufferDesc.BufferBytes = L.Length * 4;
+			secondaryBufferDesc.Format = waveFormat;
+			secondaryBufferDesc.Flags = BufferFlags.GetCurrentPosition2 | BufferFlags.ControlPositionNotify | BufferFlags.GlobalFocus |
+										BufferFlags.ControlVolume | BufferFlags.StickyFocus;
+			secondaryBufferDesc.AlgorithmFor3D = Guid.Empty;
+			secondarySoundBuffer = new SecondarySoundBuffer(directSound, secondaryBufferDesc);
+
+			// Get Capabilties from secondary sound buffer
+			var capabilities = secondarySoundBuffer.Capabilities;
+
+			// Lock the buffer
+			DataStream dataPart2;
+			var dataPart1 = secondarySoundBuffer.Lock(0, capabilities.BufferBytes, LockFlags.EntireBuffer, out dataPart2);
+
+			// Fill the buffer with some sound
+			int numberOfSamples = L.Length;
+
+			for (int i = 0; i < numberOfSamples; i++)
+			{
+				short value_l = (short)(L[i] * Int16.MaxValue);
+				short value_r = (short)(L[i] * Int16.MaxValue);
+
+				dataPart1.Write(value_l);
+				dataPart1.Write(value_r);
+			}
+
+			// Unlock the buffer
+			secondarySoundBuffer.Unlock(dataPart1, dataPart2);
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void loadwavFileToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				string wavFile = openFileDialog1.FileName;
+				PaintWavFile(wavFile);
+			}
+		}
+
+		private void toolStripButton1_Click(object sender, EventArgs e)
+		{
+			// Play the song
+			secondarySoundBuffer.Play(0, PlayFlags.Looping);
+		}
+
+		// https://stackoverflow.com/questions/8754111/how-to-read-the-data-in-a-wav-file-to-an-array
+
+		bool ReadWav(string filename, out float[] L, out float[] R)
+		{
+			L = R = null;
+
+			try
+			{
+				using (FileStream fs = File.Open(filename, FileMode.Open))
+				{
+					BinaryReader reader = new BinaryReader(fs);
+
+					// chunk 0
+					int chunkID = reader.ReadInt32();
+					int fileSize = reader.ReadInt32();
+					int riffType = reader.ReadInt32();
+
+
+					// chunk 1
+					int fmtID = reader.ReadInt32();
+					int fmtSize = reader.ReadInt32(); // bytes for this chunk (expect 16 or 18)
+
+					// 16 bytes coming...
+					int fmtCode = reader.ReadInt16();
+					int channels = reader.ReadInt16();
+					int sampleRate = reader.ReadInt32();
+					int byteRate = reader.ReadInt32();
+					int fmtBlockAlign = reader.ReadInt16();
+					int bitDepth = reader.ReadInt16();
+
+					if (fmtSize == 18)
+					{
+						// Read any extra values
+						int fmtExtraSize = reader.ReadInt16();
+						reader.ReadBytes(fmtExtraSize);
+					}
+
+					// chunk 2
+					int dataID = reader.ReadInt32();
+					int bytes = reader.ReadInt32();
+
+					// DATA!
+					byte[] byteArray = reader.ReadBytes(bytes);
+
+					int bytesForSamp = bitDepth / 8;
+					int nValues = bytes / bytesForSamp;
+
+
+					float[] asFloat = null;
+					switch (bitDepth)
+					{
+						case 64:
+							double[]
+								asDouble = new double[nValues];
+							Buffer.BlockCopy(byteArray, 0, asDouble, 0, bytes);
+							asFloat = Array.ConvertAll(asDouble, e => (float)e);
+							break;
+						case 32:
+							asFloat = new float[nValues];
+							Buffer.BlockCopy(byteArray, 0, asFloat, 0, bytes);
+							break;
+						case 16:
+							Int16[]
+								asInt16 = new Int16[nValues];
+							Buffer.BlockCopy(byteArray, 0, asInt16, 0, bytes);
+							asFloat = Array.ConvertAll(asInt16, e => e / (float)(Int16.MaxValue + 1));
+							break;
+						default:
+							return false;
+					}
+
+					switch (channels)
+					{
+						case 1:
+							L = asFloat;
+							R = null;
+							return true;
+						case 2:
+							// de-interleave
+							int nSamps = nValues / 2;
+							L = new float[nSamps];
+							R = new float[nSamps];
+							for (int s = 0, v = 0; s < nSamps; s++)
+							{
+								L[s] = asFloat[v++];
+								R[s] = asFloat[v++];
+							}
+							return true;
+						default:
+							return false;
+					}
+				}
+			}
+			catch
+			{
+				return false;
+			}
+
+			return false;
+		}
+	}
+}
