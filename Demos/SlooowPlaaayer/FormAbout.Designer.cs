23e75a4a408d3f96040f8897a02729545b80244a
ogamespec <ogamespec@gmail.com>
2023-02-05 14:17:53 +0300

    SlooowPlaaayer moved

--- Demos/SlooowPlaaayer/FormAbout.Designer.cs
+++ Demos/SlooowPlaaayer/FormAbout.Designer.cs
@@ -36,7 +36,7 @@
 			// 
 			// pictureBox1
 			// 
-			this.pictureBox1.Image = global::DSoundDemo.Properties.Resources.SlooowPlaaayer;
+			this.pictureBox1.Image = global::SlooowPlaaayer.Properties.Resources.SlooowPlaaayer;
 			this.pictureBox1.Location = new System.Drawing.Point(12, 10);
 			this.pictureBox1.Name = "pictureBox1";
 			this.pictureBox1.Size = new System.Drawing.Size(252, 189);
--- Demos/SlooowPlaaayer/Properties/Resources.Designer.cs
+++ Demos/SlooowPlaaayer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DSoundDemo.Properties {
+namespace SlooowPlaaayer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DSoundDemo.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DSoundDemo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SlooowPlaaayer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
