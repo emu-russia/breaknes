b73f8f97979c829397caef44269910642d4ee829
ogamespec <ogamespec@gmail.com>
2023-02-13 22:25:00 +0300

    Fake6502

--- BreaksAPU/NSFPlayerInterop/Fake6502.cpp
+++ BreaksAPU/NSFPlayerInterop/Fake6502.cpp
@@ -2,6 +2,8 @@
 
 #include "pch.h"
 
+using namespace BaseLogic;
+
 namespace M6502Core
 {
 	FakeM6502::FakeM6502()
@@ -10,9 +12,107 @@ namespace M6502Core
 
 	FakeM6502::~FakeM6502()
 	{
+		if (regdump != nullptr)
+		{
+			delete[] regdump;
+		}
 	}
 
 	void FakeM6502::sim(BaseLogic::TriState inputs[], BaseLogic::TriState outputs[], uint16_t* addr_bus, uint8_t* data_bus)
 	{
+		TriState PHI0 = inputs[(size_t)M6502Core::InputPad::PHI0];
+
+		TriState PHI1 = NOT(PHI0);
+		TriState PHI2 = PHI0;
+
+		// Default core mode (read address 0)
+
+		TriState RnW = TriState::One;
+		*addr_bus = 0;
+
+		// Increase the cycle counter
+
+		if (IsNegedge(PrevPHI0, PHI0))
+		{
+			phi_counter++;
+		}
+
+		// If regdump is loaded and the cycle counter has reached the value for the next RegOp - execute
+
+		if (regdump != nullptr)
+		{
+			if (phi_counter >= next_phi)
+			{
+				APULogEntry* current = GetCurrentEntry();
+
+				RnW = (current->reg & 0x80) ? TriState::One : TriState::Zero;
+				if (RnW == TriState::Zero)
+				{
+					*data_bus = current->value;
+				}
+				*addr_bus = 0x4000 | (current->reg & 0x1f);
+
+				// If the record is the last one - delete regdump
+
+				if (regdump_entry >= regdump_entry_max)
+				{
+					delete[] regdump;
+					regdump = nullptr;
+				}
+
+				// Otherwise, switch to the next record and wait until the cycle counter reaches the desired value
+
+				else
+				{
+					regdump_entry++;
+					APULogEntry* next = GetCurrentEntry();
+					next_phi = phi_counter + next->phiDelta;
+				}
+			}
+		}
+
+		// Return values of the terminals
+
+		PrevPHI0 = PHI0;
+
+		outputs[(size_t)M6502Core::OutputPad::PHI1] = PHI1;
+		outputs[(size_t)M6502Core::OutputPad::PHI2] = PHI2;
+		outputs[(size_t)M6502Core::OutputPad::RnW] = RnW;
+	}
+
+	void FakeM6502::SetRegDump(void* ptr, size_t size)
+	{
+		if (size < sizeof(APULogEntry))
+			return;
+
+		// Delete the old regdump
+
+		if (regdump != nullptr)
+		{
+			delete[] regdump;
+			regdump = nullptr;
+		}
+
+		// Copy regdump to a new buffer
+
+		regdump = new uint8_t[size];
+		memcpy(regdump, ptr, size);
+		regdump_size = size;
+
+		// Set the initial values of the state logic
+
+		regdump_entry = 0;
+		regdump_entry_max = regdump_size / sizeof(APULogEntry);
+
+		APULogEntry* first_entry = (APULogEntry*)ptr;
+		next_phi = first_entry->phiDelta;
+
+		phi_counter = 0;
+		PrevPHI0 = TriState::X;
+	}
+
+	APULogEntry* FakeM6502::GetCurrentEntry()
+	{
+		return (APULogEntry*)(regdump + regdump_entry * sizeof(APULogEntry));
 	}
 }
--- BreaksAPU/NSFPlayerInterop/Fake6502.h
+++ BreaksAPU/NSFPlayerInterop/Fake6502.h
@@ -4,12 +4,35 @@
 
 namespace M6502Core
 {
+#pragma pack(push, 1)
+	struct APULogEntry
+	{
+		uint32_t	phiDelta;	// Delta of previous PHI0 counter (CPU Core clock cycles) value at the time of accessing to the register
+		uint8_t 	reg; 		// APU register index (0-0x1f) + Flag (msb - 0: write, 1: read)
+		uint8_t 	value;		// Written value. Not used for reading.
+		uint16_t	padding;	// Not used (yet?)
+	};
+#pragma pack(pop)
+
 	class FakeM6502 : public M6502
 	{
+		uint8_t* regdump = nullptr;
+		size_t regdump_size = 0;
+		size_t regdump_entry = 0;
+		size_t regdump_entry_max = 0;
+		size_t phi_counter = 0;
+		size_t next_phi = 0;
+
+		BaseLogic::TriState PrevPHI0 = BaseLogic::TriState::X;
+
+		APULogEntry* GetCurrentEntry();
+
 	public:
 		FakeM6502();
 		~FakeM6502();
 
 		void sim(BaseLogic::TriState inputs[], BaseLogic::TriState outputs[], uint16_t* addr_bus, uint8_t* data_bus);
+
+		void SetRegDump(void* ptr, size_t size);
 	};
 }
