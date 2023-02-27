0eb6a0104e9a7457374245fdc0b70b054a50533a
ogamespec <ogamespec@gmail.com>
2023-02-27 11:09:15 +0300

    Icon

--- Breaks6502/BreaksDebug/FormAbout.Designer.cs
+++ Breaks6502/BreaksDebug/FormAbout.Designer.cs
@@ -1,122 +1,127 @@
 ﻿
 namespace BreaksDebug
 {
-    partial class FormAbout
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class FormAbout
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-			this.button1 = new System.Windows.Forms.Button();
-			this.label2 = new System.Windows.Forms.Label();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			this.SuspendLayout();
+			pictureBox1 = new PictureBox();
+			label1 = new Label();
+			linkLabel1 = new LinkLabel();
+			button1 = new Button();
+			label2 = new Label();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			SuspendLayout();
 			// 
 			// pictureBox1
 			// 
-			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-			this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(77, 73);
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
+			pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+			pictureBox1.BackgroundImageLayout = ImageLayout.None;
+			pictureBox1.Location = new Point(15, 15);
+			pictureBox1.Margin = new Padding(4, 3, 4, 3);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(90, 84);
+			pictureBox1.TabIndex = 0;
+			pictureBox1.TabStop = false;
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(96, 13);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(173, 32);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Breaks Debugger, v. 2.0\r\n© 2023, emu-russia";
+			label1.AutoSize = true;
+			label1.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+			label1.Location = new Point(112, 15);
+			label1.Margin = new Padding(4, 0, 4, 0);
+			label1.Name = "label1";
+			label1.Size = new Size(173, 32);
+			label1.TabIndex = 1;
+			label1.Text = "Breaks Debugger, v. 2.1\r\n© 2023, emu-russia";
 			// 
 			// linkLabel1
 			// 
-			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(12, 98);
-			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(187, 13);
-			this.linkLabel1.TabIndex = 2;
-			this.linkLabel1.TabStop = true;
-			this.linkLabel1.Text = "https://github.com/emu-russia/breaks";
+			linkLabel1.AutoSize = true;
+			linkLabel1.Location = new Point(14, 113);
+			linkLabel1.Margin = new Padding(4, 0, 4, 0);
+			linkLabel1.Name = "linkLabel1";
+			linkLabel1.Size = new Size(212, 15);
+			linkLabel1.TabIndex = 2;
+			linkLabel1.TabStop = true;
+			linkLabel1.Text = "https://github.com/emu-russia/breaks";
 			// 
 			// button1
 			// 
-			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.button1.Location = new System.Drawing.Point(317, 95);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(94, 30);
-			this.button1.TabIndex = 3;
-			this.button1.Text = "Close";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			button1.Location = new Point(370, 110);
+			button1.Margin = new Padding(4, 3, 4, 3);
+			button1.Name = "button1";
+			button1.Size = new Size(110, 35);
+			button1.TabIndex = 3;
+			button1.Text = "Close";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
 			// 
 			// label2
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(95, 59);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(175, 13);
-			this.label2.TabIndex = 4;
-			this.label2.Text = "6502 Data Path diagram by ttlworks";
+			label2.AutoSize = true;
+			label2.Location = new Point(111, 68);
+			label2.Margin = new Padding(4, 0, 4, 0);
+			label2.Name = "label2";
+			label2.Size = new Size(193, 15);
+			label2.TabIndex = 4;
+			label2.Text = "6502 Data Path diagram by ttlworks";
 			// 
 			// FormAbout
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(423, 137);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.linkLabel1);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.pictureBox1);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.KeyPreview = true;
-			this.MinimumSize = new System.Drawing.Size(420, 160);
-			this.Name = "FormAbout";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "About";
-			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormAbout_KeyDown);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(493, 158);
+			Controls.Add(label2);
+			Controls.Add(button1);
+			Controls.Add(linkLabel1);
+			Controls.Add(label1);
+			Controls.Add(pictureBox1);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			KeyPreview = true;
+			Margin = new Padding(4, 3, 4, 3);
+			MinimumSize = new Size(487, 179);
+			Name = "FormAbout";
+			StartPosition = FormStartPosition.CenterParent;
+			Text = "About";
+			KeyDown += FormAbout_KeyDown;
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        }
+		#endregion
 
-        #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-    }
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+	}
 }
--- Breaks6502/BreaksDebug/FormAbout.cs
+++ Breaks6502/BreaksDebug/FormAbout.cs
@@ -10,24 +10,24 @@ using System.Windows.Forms;
 
 namespace BreaksDebug
 {
-    public partial class FormAbout : Form
-    {
-        public FormAbout()
-        {
-            InitializeComponent();
-        }
+	public partial class FormAbout : Form
+	{
+		public FormAbout()
+		{
+			InitializeComponent();
+		}
 
-        private void FormAbout_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Escape)
-            {
-                Close();
-            }
-        }
+		private void FormAbout_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				Close();
+			}
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-    }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
 }
