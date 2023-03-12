889a72210e365e021a188eb294d2eb93d7dac4b9
ogamespec <ogamespec@gmail.com>
2023-03-12 15:45:26 +0300

    Sound playback support

---
+++ Breaknes/Breaknes/SoundProcessing.cs
@@ -0,0 +1,78 @@
+using SharpTools;
+using System.Reflection.Metadata;
+using System.Windows.Forms;
+
+// Since the simulation is now very slow, the following strategy is used to reproduce the sound:
+// Collect one second of sound. As it is recorded, play it back via DirectSound and clear the receive buffer.
+
+namespace Breaknes
+{
+	public class AudioRender
+	{
+		private DSound? audio_backend;
+		private int OutputSampleRate = 48000;
+		private float OutputDC = 0.0f;
+		private List<float> SampleBuf = new();
+		public bool Dma = false;       // atomic
+
+		private BreaksCore.AudioSignalFeatures aux_features;
+		private int DecimateEach = 1;
+		private int DecimateCounter = 0;
+
+		public AudioRender(System.IntPtr handle)
+		{
+			audio_backend = new DSound(handle);
+
+			// SRC
+			Redecimate();
+		}
+
+		/// <summary>
+		/// This sampler is used for SRC.
+		/// The AUX output is sampled at a high frequency, which cannot be played by a ordinary sound card.
+		/// Therefore, some of the samples are skipped to match the DSound playback frequency (SourceSampleRate variable).
+		/// </summary>
+		public void FeedSample()
+		{
+			if (DecimateCounter >= DecimateEach)
+			{
+				float sample;
+				BreaksCore.SampleAudioSignal(out sample);
+				SampleBuf.Add(sample);
+				DecimateCounter = 0;
+
+				if (SampleBuf.Count >= OutputSampleRate)
+				{
+					Playback();
+				}
+			}
+			else
+			{
+				DecimateCounter++;
+			}
+		}
+
+		/// <summary>
+		/// If you change the sampling frequency of the sound source or output frequency, you must recalculate the decimation factor.
+		/// </summary>
+		private void Redecimate()
+		{
+			BreaksCore.GetApuSignalFeatures(out aux_features);
+			DecimateEach = aux_features.SampleRate / OutputSampleRate;
+			Console.WriteLine("APUSim sample rate: {0}, DSound sample rate: {1}, decimate factor: {2}", aux_features.SampleRate, OutputSampleRate, DecimateEach);
+			DecimateCounter = 0;
+		}
+
+		private void Playback()
+		{
+			if (audio_backend != null)
+			{
+				Console.WriteLine("Play 1 second");
+				Dma = true;
+				audio_backend.PlaySampleBuf(OutputSampleRate, SampleBuf, OutputDC);
+				SampleBuf.Clear();
+				Dma = false;
+			}
+		}
+	}
+}
--- Breaknes/Breaknes/VideoProcessing.cs
+++ Breaknes/Breaknes/VideoProcessing.cs
@@ -1,4 +1,3 @@
-
 using SharpTools;
 
 namespace Breaknes
