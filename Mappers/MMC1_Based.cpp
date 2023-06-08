757192684eef943722f576098dd72556205e645b
ogamespec <ogamespec@gmail.com>
2023-06-08 22:59:21 +0300

    Update MMC1_Based.cpp

--- Mappers/MMC1_Based.cpp
+++ Mappers/MMC1_Based.cpp
@@ -10,6 +10,46 @@ namespace Mappers
 		printf("MMC1_Based::MMC1_Based()\n");
 
 		mmc = new MMC1();
+
+		NESHeader* head = (NESHeader*)nesImage;
+
+		bool trainer = (head->Flags_6 & 0b100) != 0;
+		bool wram = (head->Flags_6 & 0b10) != 0;
+
+		// Load CHR ROM
+
+		if (head->CHRSize != 0)
+		{
+			CHRSize = head->CHRSize * 0x2000;
+			CHR = new uint8_t[CHRSize];
+
+			uint8_t* chrPtr = nesImage + sizeof(NESHeader) + (trainer ? NES_TRAINER_SIZE : 0) + head->PRGSize * 0x4000;
+			memcpy(CHR, chrPtr, CHRSize);
+		}
+		else
+		{
+			CHRSize = 0x2000;
+			CHR = new uint8_t[CHRSize];
+			memset(CHR, 0, CHRSize);
+		}
+
+		// Load PRG ROM
+
+		PRGSize = head->PRGSize * 0x4000;
+		PRG = new uint8_t[PRGSize];
+
+		uint8_t* prgPtr = nesImage + sizeof(NESHeader) + (trainer ? NES_TRAINER_SIZE : 0);
+		memcpy(PRG, prgPtr, PRGSize);
+
+		// Create WRAM
+
+		if (wram) {
+			RAMSize = 0x2000;
+			RAM = new uint8_t[0x2000];
+			memset(RAM, 0, RAMSize);
+		}
+
+		valid = true;
 	}
 
 	MMC1_Based::~MMC1_Based()
@@ -45,6 +85,9 @@ namespace Mappers
 		// NES only
 		uint16_t* exp, bool& exp_dirty)
 	{
+		if (!valid)
+			return;
+
 		// MMC1
 
 		TriState mmc1_in[(size_t)MMC1_Input::Max]{};
@@ -64,24 +107,97 @@ namespace Mappers
 
 		mmc->sim(mmc1_in, mmc1_out);
 
-		//CHR_A12 = 0,
-		//CHR_A13,
-		//CHR_A14,
-		//CHR_A15,
-		//CHR_A16,
-		//PRG_A14,
-		//PRG_A15,
-		//PRG_A16,
-		//PRG_A17,
-		//SRAM_CE,
-		//PRG_nCE,
+		// PPU Part
+
+		TriState nRD = cart_in[(size_t)CartInput::nRD];
+		TriState nWR = cart_in[(size_t)CartInput::nWR];
 
 		cart_out[(size_t)CartOutput::VRAM_A10] = mmc1_out[(size_t)MMC1_Output::CIRAM_A10];
 
 		// Contains a jumper between `/PA13` and `/VRAM_CS` (? probably, just don't care for now)
 		cart_out[(size_t)CartOutput::VRAM_nCS] = cart_in[(size_t)CartInput::nPA13];
 
-		// Memory operations
+		// CHR_A13 is actually `/CS` for CHR
+		TriState nCHR_CS = FromByte((ppu_addr >> 13) & 1);
+
+		ppu_addr = (ppu_addr & 0x0fff) |			// Clear original A12+
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::CHR_A12]) << 12) |
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::CHR_A13]) << 13) |
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::CHR_A14]) << 14) |
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::CHR_A15]) << 15) |
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::CHR_A16]) << 16);
+
+		if (nCHR_CS == TriState::Zero)
+		{
+			assert(ppu_addr < CHRSize);
+
+			if (nRD == TriState::Zero)
+			{
+				uint8_t val = CHR[ppu_addr];
+
+				if (!ppu_data_dirty)
+				{
+					*ppu_data = val;
+					ppu_data_dirty = true;
+				}
+				else
+				{
+					*ppu_data = *ppu_data & val;
+				}
+			}
+
+			if (nWR == TriState::Zero)
+			{
+				CHR[ppu_addr] = *ppu_data;
+			}
+		}
+
+		// CPU Part
+
+		TriState SRAM_CE = mmc1_out[(size_t)MMC1_Output::SRAM_CE];
+		TriState PRG_nCE = mmc1_out[(size_t)MMC1_Output::PRG_nCE];
+
+		size_t prg_address = (cpu_addr & 0x3fff) |			// Clear original A14 (A15 is not routed actually)
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::PRG_A14]) << 14) |		// A14 
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::PRG_A15]) << 15) |		// A15
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::PRG_A16]) << 16) |		// A16
+			((size_t)ToByte(mmc1_out[(size_t)MMC1_Output::PRG_A17]) << 17);			// A17
+
+		if (PRG_nCE == TriState::Zero)
+		{
+			uint8_t val = PRG[prg_address];
+
+			if (!cpu_data_dirty)
+			{
+				*cpu_data = val;
+				cpu_data_dirty = true;
+			}
+			else
+			{
+				*cpu_data = *cpu_data & val;
+			}
+		}
+
+		if (SRAM_CE == TriState::One)
+		{
+			uint8_t val = RAM[prg_address & 0x1fff];
+
+			if (!cpu_data_dirty)
+			{
+				*cpu_data = val;
+				cpu_data_dirty = true;
+			}
+			else
+			{
+				*cpu_data = *cpu_data & val;
+			}
+		}
+
+		TriState nIRQ = cart_out[(size_t)CartOutput::nIRQ];
+		if (!(nIRQ == TriState::Zero || nIRQ == TriState::One))
+		{
+			cart_out[(size_t)CartOutput::nIRQ] = TriState::Z;
+		}
 
 		if (p1_type == ConnectorType::FamicomStyle && snd_out)
 		{
