3354bb24089e3950b3a109f33f72f41214ecc6fa
org <ogamespec@gmail.com>
2023-08-26 10:36:29 +0300

    more dlgs

---
+++ Breaknes/Breaknes/FormVirtualFamiController2.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Breaknes
+{
+	partial class FormVirtualFamiController2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			SuspendLayout();
+			// 
+			// FormVirtualFamiController2
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(434, 230);
+			Name = "FormVirtualFamiController2";
+			Text = "FormVirtualFamiController2";
+			ResumeLayout(false);
+		}
+
+		#endregion
+	}
+}
