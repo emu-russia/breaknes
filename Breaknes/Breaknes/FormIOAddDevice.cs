e08c3215387bd6ac17fd08180bdf3a5edc14e6c3
org <ogamespec@gmail.com>
2023-08-26 15:03:43 +0300

    Update FormIOAddDevice.cs

--- Breaknes/Breaknes/FormIOAddDevice.cs
+++ Breaknes/Breaknes/FormIOAddDevice.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Breaknes
+﻿namespace Breaknes
 {
 	public partial class FormIOAddDevice : Form
 	{
-		public IOConfigDevice device_to_add = null;
+		public IOConfigDevice? device_to_add = null;
 
 		public FormIOAddDevice()
 		{
