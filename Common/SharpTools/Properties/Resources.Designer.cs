374107fbc3c2fcbc5da00d07ea4cab960416ff35
ogamespec <ogamespec@gmail.com>
2026-04-15 18:52:57 +0300

    hardcoded paths are moved into json model

--- Common/SharpTools/Properties/Resources.Designer.cs
+++ Common/SharpTools/Properties/Resources.Designer.cs
@@ -19,10 +19,10 @@ namespace SharpTools.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace SharpTools.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SharpTools.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace SharpTools.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,10 +63,20 @@ namespace SharpTools.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap datapath {
+        public static System.Drawing.Bitmap datapath {
             get {
                 object obj = ResourceManager.GetObject("datapath", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        public static byte[] datapath_json {
+            get {
+                object obj = ResourceManager.GetObject("datapath_json", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
--- Tools/BreaksDebug/Properties/Resources.Designer.cs
+++ Tools/BreaksDebug/Properties/Resources.Designer.cs
@@ -73,6 +73,26 @@ namespace BreaksDebug.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap datapath {
+            get {
+                object obj = ResourceManager.GetObject("datapath", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] datapath_json {
+            get {
+                object obj = ResourceManager.GetObject("datapath_json", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap datapath2 {
             get {
                 object obj = ResourceManager.GetObject("datapath2", resourceCulture);
